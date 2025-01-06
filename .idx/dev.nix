a9549a4d44dc297b7ef34b2cd5c9ed3f75df32e1
marcelomarcos.g9 <marcelomarcos.g9@gmail.com>
2025-01-06 20:31:37 +0000

    Melhorias na abertura do menu dos links

---
+++ .idx/dev.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }: {
+  channel = "stable-24.05";
+
+  packages = [
+    pkgs.nodejs_20
+  ];
+
+  idx = {
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npx" "live-server" "--port=$PORT" "--host=0.0.0.0"];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
+
---
+++ script.js
@@ -0,0 +1,37 @@
+function validar () {
+    let link2 = document.getElementById('menu-marcador2');
+    let menu2 = document.getElementById('menu-2');
+    let link1 = document.getElementById('menu-marcador');
+    let menu1 = document.getElementById('menu-1');
+
+    if (link1.checked) {
+        console.log('True');
+        menu1.style.display = 'block';
+
+            console.log('Falselink2');
+            link2.checked = false;
+            menu2.style.display = 'none';
+    } else {
+        console.log('false');
+        menu1.style.display = 'none';
+    }
+}
+
+function validar2 () {
+    let link2 = document.getElementById('menu-marcador2');
+    let menu2 = document.getElementById('menu-2');
+    let link1 = document.getElementById('menu-marcador');
+    let menu1 = document.getElementById('menu-1');
+
+    if (link2.checked) {
+        console.log('True');
+        menu2.style.display = 'block';
+
+            console.log('Falselink1');
+            link1.checked = false;
+            menu1.style.display = 'none';
+    } else {
+        console.log('false');
+        menu2.style.display = 'none';
+    }
+}
