b4808d4547dce065199bfae2a16003b8c728e4f2
marcelomarcos.g9 <marcelomarcos.g9@gmail.com>
2025-01-08 20:47:57 +0000

    Prototipo da aplicação web

--- script.js
+++ script.js
@@ -32,3 +32,41 @@ function validar2 () {
         menu2.style.display = 'none';
     }
 }
+
+function abrirPagina (Valor) {
+    let link = document.getElementById('paginaPrincipal');
+    if (Valor === 1) {
+        link.src = "https://docs.google.com/forms/d/1aTdBbv2l8ZMT3B1PiIkJevYe_qB41_HtbqSHEewMw9o/viewform";
+    } else if (Valor === 2) {
+        link.src = "https://docs.google.com/forms/d/1cQ0D1ZvDEv0wTZUJAw_INdFhEVCnj04Lf_WO0L6RGkQ/viewform";
+    } else if (Valor === 3) {
+        link.src = "https://docs.google.com/forms/d/1bvhHmg4IpnRgIXZDSQ9DE2NLpMZTLhlOH-zCjk0stOY/viewform";
+    } else if (Valor === 4) {
+        link.src = "https://docs.google.com/forms/d/11E1aRR36XpDFgbAuQW66pLqkXctZ2E3H3bbJv9KusGA/viewform";
+    }
+}
+
+    let btLink = document.getElementById("btLink");
+        btLink.addEventListener("click", function(event) {
+            event.preventDefault(); // Evita o comportamento padrão do link
+            abrirPagina(1);
+        });
+
+    let btLink2 = document.getElementById("btLink2");
+    btLink2.addEventListener("click", function(event) {
+        event.preventDefault(); // Evita o comportamento padrão do link
+        abrirPagina(2);
+    });
+
+    let btLink3 = document.getElementById("btLink3");
+    btLink3.addEventListener("click", function(event) {
+        event.preventDefault(); // Evita o comportamento padrão do link
+        abrirPagina(3);
+    });
+
+    let btLink4 = document.getElementById("btLink4");
+    btLink4.addEventListener("click", function(event) {
+        event.preventDefault(); // Evita o comportamento padrão do link
+        abrirPagina(4);
+    });
+    
